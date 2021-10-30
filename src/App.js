8ff59cbd6d450cd7a9bb299705b4b232829ac2e8
Mette Vorraa <mettevorraa@gmail.com>
2021-10-30 16:21:31 +0100

    add GitHub repository link

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
         <h1>Dictionary</h1>
+        <p className="footer"><a href="https://github.com/metvor/dictionary" target="_blank" rel="noreferrer">Open-source code</a> by Mette</p>
     </div>
   );
 }
